005a60b4f5861fde1c4ac0a21dc2f409d7ffc8ef
Phillip_Chan <phillip.chan@liferay.COM>
2016-05-27 15:32:58 -0700

    Added test method

--- process-and-test.js
+++ process-and-test.js
@@ -13,7 +13,6 @@ var processAndTest = function(initialData) {
 		self.processSets.push({
 			action: options.action,
 			tests: options.tests,
-			mode: options.mode,
 			errorMessage: options.errorMessage
 		})
 	};
@@ -23,28 +22,29 @@ var processAndTest = function(initialData) {
 		loopProcesses:
 			for (var p = 0; p < self.processSets.length; p++) {
 				var currentSet = self.processSets[p];
-				
-				// regular syncronous operations
-				if (currentSet.mode !== "async") {
-					self.data = currentSet.action();
-					if (self.runTests(currentSet) === false) {
-						resultOfRun = self.generateResult(false, self.errorMessage);
-						break loopProcesses;
-					}
-					else {
-						resultOfRun = self.generateResult(true, self.data);
-					}
-				} 
-				// for async operations that leverage promises
-				else if (currentSet.mode == "async") {
-					currentSet.action(self.data)
-						.then(function(result) {
-							self.data = result;
-							self.runTests(currentSet);
-						})
+			
+				self.data = currentSet.action();
+				if (self.runTests(currentSet) === false) {
+					resultOfRun = self.generateResult(false, self.errorMessage);
+					break loopProcesses;
+				}
+				else {
+					resultOfRun = self.generateResult(true, self.data);
 				}
 			}
 		
 		return resultOfRun;
-	}
+	};
+	this.runTests = function(set) {
+		loopTests:
+			for (var i = 0; i < set.tests.length ; i++) {
+				var pass = set.tests[i](self.data);
+
+				if (pass === false) {
+					alert("i should stop shouldn't i")
+					self.errorMessage = set.errorMessage;
+					return false;
+				}
+			}
+	};
 };
