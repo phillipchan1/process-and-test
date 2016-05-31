b619ab1554bb4a93167127e3ee7878ac699c6a23
Phillip_Chan <phillip.chan@liferay.COM>
2016-05-31 15:54:14 -0700

    Source formatting

--- process-and-test.js
+++ process-and-test.js
@@ -1,80 +1,80 @@
 var processAndTest = function(initialData) {
-			var self = this;
-			this.processSets = [];
-			this.data = initialData;
-			this.errorMessage = undefined;
-			this.pass = false;
-			this.newProcess = function(options) {
-				self.processSets.push({
-					action: options.action,
-					tests: options.tests,
-					mode: options.mode,
-					errorMessage: options.errorMessage
-				})
-			};
-			this.run = function() {
-				var counter = 0;
-				var numOfProcess = self.processSets.length;
-				var currentSet = self.processSets[counter];
+	var self = this;
+	this.processSets = [];
+	this.data = initialData;
+	this.errorMessage = undefined;
+	this.pass = false;
+	this.newProcess = function(options) {
+		self.processSets.push({
+			action: options.action,
+			tests: options.tests,
+			mode: options.mode,
+			errorMessage: options.errorMessage
+		})
+	};
+	this.run = function() {
+		var counter = 0;
+		var numOfProcess = self.processSets.length;
+		var currentSet = self.processSets[counter];
 
-				// supports async operations
-				var runNextAction = function(set) {
-					// if async
-					if (currentSet.mode == "async") {
-						currentSet.action(self.data)
-							.then(function(res) {
-								self.data = res;
-								runTests(currentSet);
-							})
-					} 
-					// if sync
-					else {
-						self.data = currentSet.action(self.data);
+		// supports async operations
+		var runNextAction = function(set) {
+			// if async
+			if (currentSet.mode == "async") {
+				currentSet.action(self.data)
+					.then(function(res) {
+						self.data = res;
 						runTests(currentSet);
-					}
-				};
+					})
+			} 
+			// if sync
+			else {
+				self.data = currentSet.action(self.data);
+				runTests(currentSet);
+			}
+		};
 
-				// only sync operations
-				var runTests = function(data) {
+		// only sync operations
+		var runTests = function(data) {
 
-					// if it fails test
-					if (self.runTests(data) === false) {
-						self.pass = false;
-						self.onEnd();
-					}
-					// if current set passes test
-					else {
-						runItAgain();
-					}
-				}
-
-				// our loop/counter
-				var runItAgain = function() {
-					// should we run the next one?
-					if ((counter + 1) < numOfProcess) {
-						counter++
-						currentSet = self.processSets[counter];
-						runNextAction(currentSet);
-					}
-
-					else {
-						self.pass = true;
-						self.onEnd()
-					}
-				}
-
-				// run it for the first time.
-				runNextAction(currentSet);
-			};
-			this.onEnd = function() {},
-			this.runTests = function(set) {
-				for (var i = 0; i < set.tests.length ; i++) {
-					var testResult = set.tests[i](self.data);
-
-					if (testResult !== true) {
-						self.errorMessage = testResult;
-						return false;
-					}
-				}
-			};
+			// if it fails test
+			if (self.runTests(data) === false) {
+				self.pass = false;
+				self.onEnd();
+			}
+			// if current set passes test
+			else {
+				runItAgain();
+			}
 		}
+
+		// our loop/counter
+		var runItAgain = function() {
+			// should we run the next one?
+			if ((counter + 1) < numOfProcess) {
+				counter++
+				currentSet = self.processSets[counter];
+				runNextAction(currentSet);
+			}
+
+			else {
+				self.pass = true;
+				self.onEnd()
+			}
+		}
+
+		// run it for the first time.
+		runNextAction(currentSet);
+	};
+	this.onEnd = function() {},
+	this.runTests = function(set) {
+		for (var i = 0; i < set.tests.length ; i++) {
+			var testResult = set.tests[i](self.data);
+
+			if (testResult !== true) {
+				self.errorMessage = testResult;
+				return false;
+			}
+		}
+	};
+}
